d640e65c48df785af124dd3f3f42d9fb1c7ac707
sachin singh <sachin.singh@antino.io>
2020-05-03 00:08:59 +0530

    init commit

--- server.js
+++ server.js
@@ -18,6 +18,16 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
