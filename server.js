2b18f698a12a83b9b5a56a6a5e2f64ad21d15d68
sachin singh <sachin.singh@antino.io>
2020-05-02 23:59:23 +0530

    init commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+//connectDB
+
+connectDB();
+//init middleware
+
+app.use(express.json());
+
+//Mount route
+
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
